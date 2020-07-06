4d6cdbf8ed8156f960ff5339b2ebe1659c19c313
Gabriel Martins Pires <brgabrielmartins@gmail.com>
2020-07-06 02:05:47 -0300

    ‘Adicionando-ListView’

--- todo/lib/main.dart
+++ todo/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'models/item.dart';
 
 void main() => runApp(App());
 
@@ -16,17 +17,32 @@ class App extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  var items = new List<Item>();
+
+  HomePage() {
+    items = [];
+    items.add(Item(title: "Item1", done: false));
+    items.add(Item(title: "Item2", done: true));
+    items.add(Item(title: "Item3", done: false));
+  }
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Todo List"),
       ),
-      body: Container(
-        child: Center(
-          child: Text("Olá mundo"),
-        ),
+      body: ListView.builder(
+        itemCount: widget.items.length,
+        itemBuilder: (BuildContext ctxt, int index) {
+          return Text(widget.items[index].title);
+        },
       ),
     );
   }
